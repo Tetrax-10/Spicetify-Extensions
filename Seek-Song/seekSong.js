c85274ab208ee341550d3f28c6b7532f451880b1
Raghavan <raghavanmarch2002@gmail.com>
2022-05-28 19:09:02 +0530

    prevent scrolling

--- Seek-Song/seekSong.js
+++ Seek-Song/seekSong.js
@@ -32,6 +32,16 @@ function initSeekSong() {
     }
 
     function main() {
+        window.addEventListener(
+            "keydown",
+            function (e) {
+                if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
+                    e.preventDefault();
+                }
+            },
+            false
+        );
+
         eventTriggerWithKey("0", () => {
             Spicetify.Player.seek(0);
         });
