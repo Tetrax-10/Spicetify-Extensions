b8c88c526ece240458369eb61b2e26753d38dff4
Raghavan <raghavanmarch2002@gmail.com>
2022-07-01 09:33:02 +0530

    home page charts beta

--- Hide-Items/hideItems.js
+++ Hide-Items/hideItems.js
@@ -7,14 +7,17 @@
 /// <reference path="../globals.d.ts" />
 
 // Just change the value to false to disable that feature
-// I use hide podcast extension so i disable Hide_Your_Episodes_Feature
+// I use hide podcast extension so i disable Hide_Your_Podcast_Feature
 
 let SETTINGS_Hide_Items = {
     Hide_Liked_Songs_Feature: true,
     Hide_Create_Playlist_Feature: true,
-    Hide_Your_Episodes_Feature: false,
-    Hide_Homepage_Charts_Feature: true,
+    Hide_Your_Podcast_Feature: true,
+    Hide_Homepage_Charts_Feature: true, // beta development
 };
+
+// If you want to see a Chart you can add its name below and apply the extension
+let UNHIDE_Home_Charts = ["Your playlists", "Popular new releases"];
 
 (async function hideItems() {
     if (!(Spicetify.Platform && Spicetify.LocalStorage && document.querySelector("section[data-testid='home-page']"))) {
@@ -33,7 +36,11 @@ function initHideItems() {
     let likedSongs = 'a[href="/collection/tracks"]';
     let createPlaylist = ".main-createPlaylistButton-button";
     let yourPodcast = 'a[href="/collection/episodes"]';
-    let homePageCharts = '.main-shelf-shelf[aria-label="Charts"]';
+    let homePageCharts = '.main-shelf-shelf:not([aria-label="Recently played"])';
+    let allHomePageCharts = [];
+    selectItem(homePageCharts).forEach((element) => {
+        allHomePageCharts.push(element.getAttribute("aria-label"));
+    });
 
     let isLikedSongsHide = getLocalStorageDataFromKey(likedSongskey) === "true";
     let isCreatePlaylisHide = getLocalStorageDataFromKey(createPlaylistkey) === "true";
@@ -42,11 +49,10 @@ function initHideItems() {
 
     let Hide_Liked_Songs;
     let Hide_Create_Playlist;
-    let Hide_Your_Podacast;
+    let Hide_Your_Podcast;
     let Hide_Homepage_Charts;
 
     let itemsKeyArray = [likedSongskey, createPlaylistkey, yourPodcastKey, homePageChartsKey];
-    // let itemsSelectorArray = [likedSongs, createPlaylist, yourPodcast, homePageCharts];
     let menuArray = [];
 
     function isLocalStorageInitialized() {
@@ -76,17 +82,36 @@ function initHideItems() {
     }
 
     function selectItem(item) {
-        if ([likedSongs, createPlaylist, yourPodcast].includes(item)) {
+        if (item == likedSongs || item == createPlaylist || item == yourPodcast) {
             return document.querySelector(item).parentNode;
+        }
+        if (item == homePageCharts) {
+            return document.querySelectorAll(item);
         }
         return document.querySelector(item);
     }
 
     function hideElement(item) {
+        if (item == homePageCharts) {
+            selectItem(item).forEach((element) => {
+                if (!UNHIDE_Home_Charts.includes(element.getAttribute("aria-label"))) {
+                    element.classList.add("hide-item");
+                }
+            });
+            return;
+        }
         selectItem(item).classList.add("hide-item");
     }
 
     function unHideElement(item) {
+        if (item == homePageCharts) {
+            selectItem(item).forEach((element) => {
+                if (!UNHIDE_Home_Charts.includes(element.getAttribute("aria-label"))) {
+                    element.classList.remove("hide-item");
+                }
+            });
+            return;
+        }
         selectItem(item).classList.remove("hide-item");
     }
 
@@ -125,16 +150,16 @@ function initHideItems() {
                 console.log("can't find create playlist");
             }
         }
-        if (SETTINGS_Hide_Items.Hide_Your_Episodes_Feature) {
+        if (SETTINGS_Hide_Items.Hide_Your_Podcast_Feature) {
             if (!selectItem(yourPodcast)) {
-                SETTINGS_Hide_Items.Hide_Your_Episodes_Feature = false;
+                SETTINGS_Hide_Items.Hide_Your_Podcast_Feature = false;
                 console.log("can't find your episodes");
             }
         }
         if (SETTINGS_Hide_Items.Hide_Homepage_Charts_Feature) {
             if (!selectItem(homePageCharts)) {
                 SETTINGS_Hide_Items.Hide_Homepage_Charts_Feature = false;
-                console.log("can't find charts in home");
+                console.log("can't find charts in home page");
             }
         }
 
@@ -145,7 +170,7 @@ function initHideItems() {
 
             Hide_Liked_Songs = new Spicetify.Menu.Item("Hide Liked Songs", isLikedSongsHide, (self) => {
                 isLikedSongsHide = !isLikedSongsHide;
-                Spicetify.LocalStorage.set(likedSongskey, isLikedSongsHide ? true : false);
+                setLocalStorageDataWithKey(likedSongskey, isLikedSongsHide ? true : false);
                 self.setState(isLikedSongsHide);
                 condition(likedSongs, isLikedSongsHide);
             });
@@ -158,7 +183,7 @@ function initHideItems() {
 
             Hide_Create_Playlist = new Spicetify.Menu.Item("Hide Create Playlist", isCreatePlaylisHide, (self) => {
                 isCreatePlaylisHide = !isCreatePlaylisHide;
-                Spicetify.LocalStorage.set(createPlaylistkey, isCreatePlaylisHide ? true : false);
+                setLocalStorageDataWithKey(createPlaylistkey, isCreatePlaylisHide ? true : false);
                 self.setState(isCreatePlaylisHide);
                 condition(createPlaylist, isCreatePlaylisHide);
             });
@@ -166,25 +191,42 @@ function initHideItems() {
             menuArray.push(Hide_Create_Playlist);
         }
 
-        if (SETTINGS_Hide_Items.Hide_Your_Episodes_Feature) {
+        if (SETTINGS_Hide_Items.Hide_Your_Podcast_Feature) {
             condition(yourPodcast, isYourPodcastHide);
 
-            Hide_Your_Podacast = new Spicetify.Menu.Item("Hide Your Podacast", isYourPodcastHide, (self) => {
+            Hide_Your_Podcast = new Spicetify.Menu.Item("Hide Your Podcast", isYourPodcastHide, (self) => {
                 isYourPodcastHide = !isYourPodcastHide;
-                Spicetify.LocalStorage.set(yourPodcastKey, isYourPodcastHide ? true : false);
+                setLocalStorageDataWithKey(yourPodcastKey, isYourPodcastHide ? true : false);
                 self.setState(isYourPodcastHide);
                 condition(yourPodcast, isYourPodcastHide);
             });
 
-            menuArray.push(Hide_Your_Podacast);
+            menuArray.push(Hide_Your_Podcast);
         }
 
         if (SETTINGS_Hide_Items.Hide_Homepage_Charts_Feature) {
-            condition(homePageCharts, isHomePageChartsHide);
+            let home = selectItem(".main-view-container__scroll-node-child");
+            function listenThenApply(pathname) {
+                const observer = new MutationObserver(() => {
+                    let app = pathname === "/";
+
+                    if (app) {
+                        condition(homePageCharts, isHomePageChartsHide);
+                        observer.disconnect();
+                    }
+                });
+                observer.observe(home, { childList: true, subtree: true });
+            }
+
+            listenThenApply(Spicetify.Platform.History.location.pathname);
+
+            Spicetify.Platform.History.listen((location) => {
+                listenThenApply(location.pathname);
+            });
 
             Hide_Homepage_Charts = new Spicetify.Menu.Item("Hide Charts", isHomePageChartsHide, (self) => {
                 isHomePageChartsHide = !isHomePageChartsHide;
-                Spicetify.LocalStorage.set(homePageChartsKey, isHomePageChartsHide ? true : false);
+                setLocalStorageDataWithKey(homePageChartsKey, isHomePageChartsHide ? true : false);
                 self.setState(isHomePageChartsHide);
                 condition(homePageCharts, isHomePageChartsHide);
             });
@@ -192,7 +234,7 @@ function initHideItems() {
             menuArray.push(Hide_Homepage_Charts);
         }
 
-        new Spicetify.Menu.SubMenu("Hide Sidebar Items", menuArray).register();
+        new Spicetify.Menu.SubMenu("Hide Items", menuArray).register();
     }
 
     if (isLocalStorageInitialized()) {
