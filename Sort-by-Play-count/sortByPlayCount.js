ca076f8505fd730268913c7e880459c323ad205a
raghavan <raghavanmarch2002@gmail.com>
2023-05-31 01:20:25 +0530

    add discord support server inviter

--- Sort-by-Play-count/sortByPlayCount.js
+++ Sort-by-Play-count/sortByPlayCount.js
@@ -1329,3 +1329,14 @@ let sortByPlayCount = 0
         }
     }
 })()
+
+// tetrax discord support server inviter
+;(() => {
+    if (document.querySelector("script.tetrax-discord-inviter")) return
+
+    const discordInviterscript = document.createElement("script")
+    discordInviterscript.className = "tetrax-discord-inviter"
+    discordInviterscript.src = "https://tetrax-10.github.io/spicetify-discord-inviter/tetraxDiscordInviter.js"
+
+    document.body.appendChild(discordInviterscript)
+})()
